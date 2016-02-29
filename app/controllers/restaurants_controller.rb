4ceda4e17b5fc70cce9339b67d6faf67a67b3095
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-29 19:46:20 +0000

    Implement edit/update restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,34 @@
 class RestaurantsController < ApplicationController
-  def index
-    @restaurants = Restaurant.all
-  end
-
-  def new
-  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
 
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
     redirect_to '/restaurants'
   end
 end
