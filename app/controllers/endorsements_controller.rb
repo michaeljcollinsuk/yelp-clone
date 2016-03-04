b0771f07de31a72f73e0f6d1d59df6b83cbabf9a
Michael Collins <michaelj.collinsuk@gmail.com>
2016-03-04 11:36:00 +0000

    Endorsements not updating using json

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,8 +1,10 @@
 class EndorsementsController < ApplicationController
 
   def create
+    p params
+    byebug
     @review = Review.find(params[:review_id])
     @review.endorsements.create
-    redirect_to restaurants_path
+    render json: { new_endorsement_count: @review.endorsements.count }
   end
 end
