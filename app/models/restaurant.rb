f70072794e444f1f9cd02fd3410a13f780a5fac1
Michael Collins <michaelj.collinsuk@gmail.com>
2016-03-04 14:13:53 +0000

    Implement ability to add picture to restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,10 @@ class Restaurant < ActiveRecord::Base
   belongs_to :user
   validates :name, length: {minimum: 3}, uniqueness: true
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
   def average_rating
     return 'n/a' if reviews.none?
     (reviews.inject(0) {|memo, review| memo + review.rating } / reviews.length).to_i
